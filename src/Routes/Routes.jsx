aa50505d6c7f90c9e1554cc789b92f2dadeb1495
istiaquerijve <istiaquerijve@gmail.com>
2025-10-08 09:00:18 +0600

    navbar

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from '../pages/Root/Root';
+import Error from '../pages/Error/Error';
+import Home from '../pages/Home/Home';
+
+export  const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <Error></Error>,
+    children: [
+        {
+            index: true,
+            path: "/",
+            Component: Home,
+        }
+    ]
+  },
+]);
